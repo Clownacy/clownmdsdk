76f2fb14f612844edfbe6d63cf714c3fdb91294a
Clownacy <Clownacy@users.noreply.github.com>
2024-08-30 22:40:50 +0100

    Fix linking the `stdlib.h` functions from C++ code.

--- stage3/headers-and-scripts/include/stdlib.h
+++ stage3/headers-and-scripts/include/stdlib.h
@@ -3,9 +3,17 @@
 
 #include <stddef.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int abs(int i);
 
 void* memcpy(void *dest, const void *src, size_t count);
 void* memset(void *dest, int ch, size_t count);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* STDLIB_H */
