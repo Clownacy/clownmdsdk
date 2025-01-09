2776959c8de39aac2cb12b4523af539bf67ac4c5
Clownacy <Clownacy@users.noreply.github.com>
2025-01-09 13:32:14 +0000

    Add 'template-cartridge' example.

---
+++ example/template-cartridge/main.cpp
@@ -0,0 +1,207 @@
+// Copyright (c) 2025 Clownacy
+
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted.
+
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+// REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+// AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+// INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+// LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+// OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+// PERFORMANCE OF THIS SOFTWARE.
+
+#include <clownmdsdk.h>
+
+// M68000 exception handlers.
+void _BusErrorHandler()
+{
+
+}
+
+void _AddressErrorHandler()
+{
+
+}
+
+void _IllegalInstructionHandler()
+{
+
+}
+
+void _DivisionByZeroHandler()
+{
+
+}
+
+void _CHKHandler()
+{
+
+}
+
+void _TRAPVHandler()
+{
+
+}
+
+void _PrivilegeViolationHandler()
+{
+
+}
+
+void _TraceHandler()
+{
+
+}
+
+void _UnimplementedInstructionLineAHandler()
+{
+
+}
+
+void _UnimplementedInstructionLineFHandler()
+{
+
+}
+
+void _UnassignedHandler()
+{
+
+}
+
+void _UninitialisedInterruptHandler()
+{
+
+}
+
+void _SpuriousInterruptHandler()
+{
+
+}
+
+void _Level1InterruptHandler()
+{
+
+}
+
+void _Level2InterruptHandler()
+{
+
+}
+
+void _Level3InterruptHandler()
+{
+
+}
+
+void _Level4InterruptHandler()
+{
+
+}
+
+void _Level5InterruptHandler()
+{
+
+}
+
+// Runs once per frame, either 50 or 60 times a second for PAL or NTSC respectively.
+void _Level6InterruptHandler()
+{
+
+}
+
+void _Level7InterruptHandler()
+{
+
+}
+
+void _TRAP0Handler()
+{
+
+}
+
+void _TRAP1Handler()
+{
+
+}
+
+void _TRAP2Handler()
+{
+
+}
+
+void _TRAP3Handler()
+{
+
+}
+
+void _TRAP4Handler()
+{
+
+}
+
+void _TRAP5Handler()
+{
+
+}
+
+void _TRAP6Handler()
+{
+
+}
+
+void _TRAP7Handler()
+{
+
+}
+
+void _TRAP8Handler()
+{
+
+}
+
+void _TRAP9Handler()
+{
+
+}
+
+void _TRAP10Handler()
+{
+
+}
+
+void _TRAP11Handler()
+{
+
+}
+
+void _TRAP12Handler()
+{
+
+}
+
+void _TRAP13Handler()
+{
+
+}
+
+void _TRAP14Handler()
+{
+
+}
+
+void _TRAP15Handler()
+{
+
+}
+
+// Run indefinitely; should not return. Handles the bulk of operations.
+void _EntryPoint()
+{
+	// Do initialisation here.
+
+	for (;;)
+	{
+		// Run main-loop logic here.
+	}
+}
