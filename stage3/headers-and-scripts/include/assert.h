141eab102e84e7909f17d167af863553d229b1c1
Clownacy <Clownacy@users.noreply.github.com>
2025-05-04 15:42:35 +0100

    Make assert prettier and add nonstandard function for providing a message.

--- stage3/headers-and-scripts/include/assert.h
+++ stage3/headers-and-scripts/include/assert.h
@@ -3,7 +3,7 @@
 
 /* TODO: Proper assert handling. */
 #ifdef NDEBUG
-#define assert(x)
+#define assert(x) ((void)0)
 #else
 #define assert(x) \
 	do \
@@ -11,11 +11,14 @@
 		if (!(x)) \
 		{ \
 			/* TODO: What do we do about the SubCPU? */ \
-			ClownMDSDK::MainCPU::Debug::PrintLine(__func__, " at " __FILE__ ":", __LINE__, " - Assertion '" #x "' failed."); \
+			ClownMDSDK::MainCPU::Debug::PrintLine(__func__, "() at " __FILE__ ":", __LINE__); \
+			ClownMDSDK::MainCPU::Debug::PrintLine("    Assertion '" #x "' failed."); \
 			asm("illegal"); \
 		} \
 	} while(0)
 #endif
+
+#define assertm(x, message) assert((x) && message)
 
 #include <clownmdsdk.h>
 
