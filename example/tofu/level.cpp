723a3bc0c9c3a44fd0609b91ca4e645e6dd70a8a
Clownacy <Clownacy@users.noreply.github.com>
2025-05-04 22:55:50 +0100

    Merge DrawRow with DrawRows.

--- example/tofu/level.cpp
+++ example/tofu/level.cpp
@@ -19,55 +19,8 @@ const std::array<unsigned char, level_width_in_blocks * level_height_in_blocks> 
 Coordinate::Pixel camera;
 static Coordinate::Block camera_previous;
 
-static void DrawRow(
-	Z80::Bus &z80_bus,
-	const Coordinate::Block &block_row_position,
-	unsigned int first_transfer_vram_address,
-	unsigned int second_transfer_vram_address,
-	const unsigned int first_transfer_length,
-	const unsigned int second_transfer_length
-)
-{
-	std::array<std::array<VDP::VRAM::TileMetadata, line_length_in_blocks * Coordinate::block_width_in_tiles>, Coordinate::block_height_in_tiles> tile_metadata_lines;
-	VDP::VRAM::TileMetadata tile_metadata{.priority = false, .palette_line = 0, .y_flip = false, .x_flip = false, .tile_index = 0};
-
-	auto *tile = &GetBlock(block_row_position);
-
-	unsigned int tile_in_screen_x = 0;
-
-	for (unsigned int block_in_screen_x = 0; block_in_screen_x < line_length_in_blocks; ++block_in_screen_x)
-	{
-		tile_metadata.tile_index = *tile++ * Coordinate::block_width_in_tiles * Coordinate::block_height_in_tiles;
-
-		for (unsigned int tile_in_block_x = 0; tile_in_block_x < Coordinate::block_width_in_tiles; ++tile_in_block_x)
-		{
-			for (unsigned int tile_in_block_y = 0; tile_in_block_y < Coordinate::block_height_in_tiles; ++tile_in_block_y)
-			{
-				tile_metadata_lines[tile_in_block_y][tile_in_screen_x] = tile_metadata;
-				++tile_metadata.tile_index;
-			}
-
-			++tile_in_screen_x;
-		}
-	}
-
-	for (unsigned int tile_in_block_y = 0; tile_in_block_y < Coordinate::block_height_in_tiles; ++tile_in_block_y)
-	{
-		z80_bus.CopyWordsToVDPWithDMA(VDP::RAM::VRAM, first_transfer_vram_address, &tile_metadata_lines[tile_in_block_y][0], first_transfer_length );
-		first_transfer_vram_address += sizeof(VDP::VRAM::TileMetadata) * plane_size_in_tiles.x;
-
-		if (second_transfer_length != 0)
-		{
-			z80_bus.CopyWordsToVDPWithDMA(VDP::RAM::VRAM, second_transfer_vram_address, &tile_metadata_lines[tile_in_block_y][first_transfer_length], second_transfer_length);
-			second_transfer_vram_address += sizeof(VDP::VRAM::TileMetadata) * plane_size_in_tiles.x;
-		}
-	}
-}
-
 static void DrawRows(Z80::Bus &z80_bus, const Coordinate::Block &starting_block_position, const unsigned int total_rows)
 {
-	const auto plane_block_row_size_in_word = sizeof(VDP::VRAM::TileMetadata) * plane_size_in_tiles.x * Coordinate::block_height_in_tiles;
-
 	const auto starting_tile_position_in_plane = starting_block_position.ToTile() % plane_size_in_tiles;
 
 	// We split the transfer in two to handle wrapping around the plane.
@@ -80,9 +33,40 @@ static void DrawRows(Z80::Bus &z80_bus, const Coordinate::Block &starting_block_
 
 	for (unsigned int block_in_screen_y = 0; block_in_screen_y < total_rows; ++block_in_screen_y)
 	{
-		DrawRow(z80_bus, {starting_block_position + Coordinate::Block(0, block_in_screen_y)}, first_transfer_vram_address, second_transfer_vram_address, first_transfer_length, second_transfer_length);
-		first_transfer_vram_address += plane_block_row_size_in_word;
-		second_transfer_vram_address += plane_block_row_size_in_word;
+		std::array<std::array<VDP::VRAM::TileMetadata, line_length_in_blocks * Coordinate::block_width_in_tiles>, Coordinate::block_height_in_tiles> tile_metadata_lines;
+		VDP::VRAM::TileMetadata tile_metadata{.priority = false, .palette_line = 0, .y_flip = false, .x_flip = false, .tile_index = 0};
+
+		auto *tile = &GetBlock(starting_block_position + Coordinate::Block(0, block_in_screen_y));
+
+		unsigned int tile_in_screen_x = 0;
+
+		for (unsigned int block_in_screen_x = 0; block_in_screen_x < line_length_in_blocks; ++block_in_screen_x)
+		{
+			tile_metadata.tile_index = *tile++ * Coordinate::block_width_in_tiles * Coordinate::block_height_in_tiles;
+
+			for (unsigned int tile_in_block_x = 0; tile_in_block_x < Coordinate::block_width_in_tiles; ++tile_in_block_x)
+			{
+				for (unsigned int tile_in_block_y = 0; tile_in_block_y < Coordinate::block_height_in_tiles; ++tile_in_block_y)
+				{
+					tile_metadata_lines[tile_in_block_y][tile_in_screen_x] = tile_metadata;
+					++tile_metadata.tile_index;
+				}
+
+				++tile_in_screen_x;
+			}
+		}
+
+		for (unsigned int tile_in_block_y = 0; tile_in_block_y < Coordinate::block_height_in_tiles; ++tile_in_block_y)
+		{
+			z80_bus.CopyWordsToVDPWithDMA(VDP::RAM::VRAM, first_transfer_vram_address, &tile_metadata_lines[tile_in_block_y][0], first_transfer_length );
+			first_transfer_vram_address += sizeof(VDP::VRAM::TileMetadata) * plane_size_in_tiles.x;
+
+			if (second_transfer_length != 0)
+			{
+				z80_bus.CopyWordsToVDPWithDMA(VDP::RAM::VRAM, second_transfer_vram_address, &tile_metadata_lines[tile_in_block_y][first_transfer_length], second_transfer_length);
+				second_transfer_vram_address += sizeof(VDP::VRAM::TileMetadata) * plane_size_in_tiles.x;
+			}
+		}
 	}
 }
 
