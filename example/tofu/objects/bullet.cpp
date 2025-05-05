47baf06664bfe5e40b6d6d3d4dd193cd4b1a1a6e
Clownacy <Clownacy@users.noreply.github.com>
2025-05-05 02:17:45 +0100

    Tofu: Make bullets despawn upon hitting something.

--- example/tofu/objects/bullet.cpp
+++ example/tofu/objects/bullet.cpp
@@ -1,10 +1,12 @@
 #include "bullet.h"
 
+#include "../level.h"
+
 bool Objects::Bullet::Update()
 {
 	position.x += x_velocity;
 
 	QueueForDisplay(0, 0, 0, 0, {.priority = false, .palette_line = 0, .y_flip = false, .x_flip = false, .tile_index = 16});
 
-	return --life != 0;
+	return --life != 0 && Level::GetBlock(position) == 0;
 }
