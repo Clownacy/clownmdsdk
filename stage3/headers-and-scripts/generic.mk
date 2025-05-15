a041d4a86841836cbcae3285fc030affb26f12bb
Clownacy <Clownacy@users.noreply.github.com>
2025-05-15 23:25:12 +0100

    Add an explanation for the usage of `-fno-use-cxa-atexit`.
    From the commit message of 8499874628de3e19d48fbb400b401ef72e183fa2.

--- stage3/headers-and-scripts/generic.mk
+++ stage3/headers-and-scripts/generic.mk
@@ -8,6 +8,13 @@ CPP := $(CLOWNMDSDK_PREFIX)cpp
 LD := $(CLOWNMDSDK_PREFIX)ld
 INCBIN := $(CLOWNMDSDK_PREFIX)objcopy -I binary -O elf32-m68k --rename-section .data=.rodata,alloc,load,readonly,data,contents
 
+# GCC by default uses `-fuse-cxa-atexit`, which requires that the C
+# standard library support `__cxa_atexit`. Since ClownMDSDK does not
+# support `__cxa_atexit`, this setting causes errors. To mitigate this,
+# use `-fno-use-cxa-atexit` to disable this requirement. Static
+# destructors are stripped-out by the linker anyway (since Mega Drive
+# games never exit), so it doesn't matter what they do so long as they
+# don't produce errors.
 CLOWNMDSDK_CANDCXXFLAGS := -mshort -D__MEGA_DRIVE__ -ffreestanding -nodefaultlibs -fno-ident -isystem $(CLOWNMDSDK_LOCATION)/include -L $(CLOWNMDSDK_LOCATION)/lib
 CFLAGS := $(CLOWNMDSDK_CANDCXXFLAGS)
 CXXFLAGS := $(CLOWNMDSDK_CANDCXXFLAGS) -fno-exceptions -fno-rtti -fno-use-cxa-atexit
