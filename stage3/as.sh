409399ee885ed635c897482d4119d133de89e1e6
Clownacy <Clownacy@users.noreply.github.com>
2026-05-03 14:59:24 +0100

    Fix ASL not being able to find its message files.

--- stage3/as.sh
+++ stage3/as.sh
@@ -1,6 +1,6 @@
 export PREFIX="/opt/clownmdsdk"
 
 mkdir -p build
-cmake -B build asl-releases -DCMAKE_BUILD_TYPE=Release -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON
+cmake -B build asl-releases -DCMAKE_BUILD_TYPE=Release -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON -DCMAKE_INSTALL_PREFIX=$PREFIX
 cmake --build build --config Release --parallel ${1:-$(nproc)}
-cmake --install build --config Release --strip --prefix $PREFIX
+cmake --install build --config Release --strip
--- stage4/misc.sh
+++ stage4/misc.sh
@@ -15,9 +15,10 @@ mkdir -p bin/clownlzss-host
 cmake -B bin/clownlzss-host ../../clownlzss \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
-    -DCMAKE_POLICY_DEFAULT_CMP0069=NEW
+    -DCMAKE_POLICY_DEFAULT_CMP0069=NEW \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX
 cmake --build bin/clownlzss-host --config Release --parallel ${1:-$(nproc)}
-cmake --install bin/clownlzss-host --config Release --strip --prefix $PREFIX
+cmake --install bin/clownlzss-host --config Release --strip
 
 # Install target build of ClownLZSS, for its decompression libraries.
 mkdir -p bin/clownlzss-target
@@ -26,7 +27,8 @@ cmake -B bin/clownlzss-target ../../clownlzss \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
     -DCMAKE_POLICY_DEFAULT_CMP0069=NEW \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX/m68k-elf \
     -DCLOWNLZSS_TOOL=OFF \
     -DCLOWNLZSS_COMPRESSORS=OFF
 cmake --build bin/clownlzss-target --config Release --parallel ${1:-$(nproc)}
-cmake --install bin/clownlzss-target --config Release --strip --prefix $PREFIX/m68k-elf
+cmake --install bin/clownlzss-target --config Release --strip
