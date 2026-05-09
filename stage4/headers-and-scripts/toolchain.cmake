1036045a404929e2a1bbcbb80d30697f87890f25
Clownacy <Clownacy@users.noreply.github.com>
2026-05-09 14:32:28 +0100

    Streamline CMake toolchain file.
    No more messy, clunky `CMAKE_C_FLAGS` bollocks.

--- stage4/headers-and-scripts/toolchain.cmake
+++ stage4/headers-and-scripts/toolchain.cmake
@@ -20,20 +20,23 @@ set(CMAKE_CXX_COMPILER "${CLOWNMDSDK_LOCATION}/bin/m68k-elf-g++${CMAKE_HOST_EXEC
 # don't produce errors.
 # Visibility is set to hidden to assist compiler optimisations, since
 # visible symbols cannot be inlined.
-set(CMAKE_C_AND_CXX_FLAGS_INIT "-mshort -D__MEGA_DRIVE__=1 -D__CLOWNMDSDK__=1 -ffreestanding -nodefaultlibs -fno-ident -fvisibility=hidden -isystem ${CLOWNMDSDK_LOCATION}/include -L ${CLOWNMDSDK_LOCATION}/lib")
-set(CMAKE_C_FLAGS_INIT "${CMAKE_C_AND_CXX_FLAGS_INIT}")
-set(CMAKE_CXX_FLAGS_INIT "${CMAKE_C_AND_CXX_FLAGS_INIT} -fno-exceptions -fno-rtti -fno-use-cxa-atexit")
-set(CMAKE_C_STANDARD_LIBRARIES "-lgcc -lc")
-set(CMAKE_CXX_STANDARD_LIBRARIES "-lgcc -lc")
-set(CMAKE_EXE_LINKER_FLAGS_INIT "-save-temps=obj")
+add_compile_definitions(__MEGA_DRIVE__=1 __CLOWNMDSDK__=1)
+add_compile_options(-mshort -ffreestanding -nodefaultlibs -fno-ident -fvisibility=hidden)
+add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-fno-exceptions> $<$<COMPILE_LANGUAGE:CXX>:-fno-rtti> $<$<COMPILE_LANGUAGE:CXX>:-fno-use-cxa-atexit>)
+
+macro(clownmdsdk_do_language_flags language)
+	set(CMAKE_${language}_STANDARD_INCLUDE_DIRECTORIES "${CLOWNMDSDK_LOCATION}/include")
+	set(CMAKE_${language}_STANDARD_LINK_DIRECTORIES "${CLOWNMDSDK_LOCATION}/lib")
+	set(CMAKE_EXECUTABLE_SUFFIX_${language} ".bin")
+endmacro()
+
+clownmdsdk_do_language_flags(C)
+clownmdsdk_do_language_flags(CXX)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
-
-set(CMAKE_EXECUTABLE_SUFFIX_C ".bin")
-set(CMAKE_EXECUTABLE_SUFFIX_CXX ".bin")
 
 # Enable link-time optimisations for Release builds, because the
 # Mega Drive sure does need it...
@@ -42,25 +45,25 @@ set(CMAKE_POLICY_DEFAULT_CMP0069 NEW)
 
 function(clownmdsdk_add_executable name)
 	add_executable(${ARGV})
-	target_link_options(${name} PUBLIC "LINKER:-Map=${CMAKE_CURRENT_BINARY_DIR}/${name}.map")
+	target_link_options(${name} PUBLIC -save-temps=obj "LINKER:-Map=${CMAKE_CURRENT_BINARY_DIR}/${name}.map")
 endfunction()
 
 function(add_cartridge_executable name)
 	clownmdsdk_add_executable(${ARGV})
-	target_compile_definitions(${name} PRIVATE "__CLOWNMDSDK_CARTRIDGE__=1")
+	target_compile_definitions(${name} PRIVATE __CLOWNMDSDK_CARTRIDGE__=1)
 	target_link_options(${name} PUBLIC "-T${CLOWNMDSDK_LOCATION}/cartridge.ld")
 	target_link_libraries(${name} PUBLIC stubs-cartridge)
 endfunction()
 
 function(add_ip_executable name)
 	clownmdsdk_add_executable(${ARGV})
-	target_compile_definitions(${name} PRIVATE "__CLOWNMDSDK_IP__=1")
+	target_compile_definitions(${name} PRIVATE __CLOWNMDSDK_IP__=1)
 	target_link_options(${name} PUBLIC "-T${CLOWNMDSDK_LOCATION}/ip.ld")
 endfunction()
 
 function(add_sp_executable name)
 	clownmdsdk_add_executable(${ARGV})
-	target_compile_definitions(${name} PRIVATE "__CLOWNMDSDK_SP__=1")
+	target_compile_definitions(${name} PRIVATE __CLOWNMDSDK_SP__=1)
 	target_link_options(${name} PUBLIC "-T${CLOWNMDSDK_LOCATION}/sp.ld")
 endfunction()
 
---
+++ stage4/src/libm.c
@@ -0,0 +1,2 @@
+/* We just need a dummy libm so that the linker throws no errors. */
+extern int _dummy;
